a5af57d01db6a68bfa7471db966b3b448e94ced1
Stefan <stefanboblic@gmail.com>
2021-12-23 15:12:29 +0200

    commit project with created structure

---
+++ src/main/java/com/CodingChallengeSpringApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingChallengeSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodingChallengeSpringApplication.class, args);
+    }
+
+}
